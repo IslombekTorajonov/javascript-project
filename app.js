70b3b0711ffa30460361dc448c178551740a95b9
developer <90696696+IslombekTorajonov@users.noreply.github.com>
2021-11-28 00:11:52 +0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
-new fullpage('#fullpage',{
-	css3: true,
+new fullpage('#fullpage', {
+    //options here
+    css3: true,
+    autoScrolling: true,
+    scrollHorizontally: true,
+    sectionsColor: ['#160661', '#4A1569', "#562FB9", "#020041"],
 
-	autoScrolling: true,
-	scrollHorizontally:true,
-	sectionsColor:['#160661','#4a1560','#562fb9','#020041']
 });
+// fullpage_api.setAllowScrolling(true);
+
 function toRight() {
     anime({
         targets: '.toRight',
@@ -12,7 +15,33 @@ function toRight() {
             { translateX: [-500, 0] },
             // { translateX: 0 },
         ],
-        duration: 2000
+        duration: 3000
     });
 }
 toRight()
+
+function scale() {
+    anime({
+        targets: ["img"],
+        scale: [1.1, 1],
+        easing: "linear",
+        transition: "all 0.3s"
+    })
+}
+
+var app = document.getElementById('app');
+
+var typewriter = new Typewriter(app, {
+    loop: true,
+    cursor: "_"
+});
+
+typewriter.typeString('Веб-дизайн')
+    .pauseFor(2500)
+    .deleteAll()
+    .typeString('UX-дизайн')
+    .pauseFor(2500)
+    .deleteAll()
+    .typeString('UI-дизайн')
+    .pauseFor(2500)
+    .start();
